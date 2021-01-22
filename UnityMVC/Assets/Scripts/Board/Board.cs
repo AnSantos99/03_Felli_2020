747f1cc821ef51575e4d0a967d0a414aa03d8b98
AnSantos99 <an.santos95@hotmail.com>
2021-01-22 20:33:04 +0000

    Add Unity Project

---
+++ UnityMVC/Assets/Scripts/Board/Board.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+    private int waypointIndex = 0; //number of current waypoint the player ocupies
+
+    private bool movePossible = false;
+
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position; //player in first waypoint
+    }
+
+    private void Update()
+    {
+        if(movePossible)  //move if it house is empty
+        {
+            MovePiece();
+        }
+    }
+
+    private void MovePiece()
+    {
+        if(waypointIndex <= waypoints.Length -1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, 
+                waypoints[waypointIndex].transform.position, 
+                moveSpeed * Time.deltaTime);
+
+            if(transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+    }
+}
---
+++ UnityMVC/Assets/Scripts/Board/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public Image mOutlineImage;  //Reference to child image
+
+    public Vector2Int mBoardPosition = Vector2Int.zero; //Space to store the valu of where the Cell is going to be on the Board
+    public Board mBoard = null;
+    public RectTransform mRectTransform = null; //Reference to RectTransform for the Board
+
+    public void SetUp(Vector2Int newBoardPosition,Board newBoard)  //called when obj is created & handling info given by the board
+    {
+        mBoardPosition = newBoardPosition;  
+        mBoard = newBoard;
+
+        mRectTransform = GetComponent<RectTransform>();
+    }
+}
